2905991621497ae6b39803d315843c78a47df1ba
kw8799 <kw8799@gmail.com>
2017-09-19 07:01:47 +0900

    edit 170918/0918.js add .on() method

--- 170918/0918.js
+++ 170918/0918.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   var tab = $('.tab')
 
-  tab.click(function() {
-    $(this).parent().addClass('active').siblings().removeClass('active');
-  });
-  tab.focusin(function() {
+  //   tab.click(function() {
+  //     $(this).parent().addClass('active').siblings().removeClass('active');
+  //   });
+  //   tab.focusin(function() {
+  //     $(this).parent().addClass('active').siblings().removeClass('active');
+  //   })
+
+  tab.on('click focusin', function() {
     $(this).parent().addClass('active').siblings().removeClass('active');
   })
 });
